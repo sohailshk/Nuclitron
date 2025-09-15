d388264ca6ee0c6f965f23ffcf9eec574d3bc2cc
Durvesh Gawde <drvsh.vg958@gmail.com>
2025-09-15 21:34:01 +0530

    final hopefully

--- cleaned_repo/argo-conversational-platform/src/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Argo Conversational Platform - Main Package"""
-
-__version__ = "1.0.0"
-__author__ = "Argo Platform Team"
-__description__ = "AI-powered conversational platform for Argo ocean data"
